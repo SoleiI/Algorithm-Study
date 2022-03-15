1cb7acbe7614b0a6c885ff5cd16e960014531cc9
SoleiI <kmssun0524@sookmyung.ac.kr>
2022-03-15 18:56:00 +0900

    Fix input function which causes timeout in bj_1946.py

--- Week2_Greedy/bj_1946.py
+++ Week2_Greedy/bj_1946.py
@@ -2,15 +2,15 @@
 # 입력 : 테스트 케이스 개수, 각 테스트 케이스의 지원자 숫자, 서류심사 성적, 면접 성적 순위
 # 출력 : 고용 가능한 최대 인원 수
 
-testcase = int(input())     # 테스트 케이스 수
+import sys
+testcase = int(sys.stdin.readline())     # 테스트 케이스 수
 
 # 테스트케이스 별 선발 결과
 for i in range(testcase):
   applicants = int(input())   # 지원자 수
   scores = []                 # 점수
   for i in range(applicants):
-    score1, score2 = map(int, input().split())
-    scores.append([score1, score2])
+    scores.append(list(map(int, sys.stdin.readline().split())))
 
   scores.sort()
   score = scores[0][1]
